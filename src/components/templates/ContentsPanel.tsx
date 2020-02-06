0bfdc8434e9acf843152793415c11af17a11d502
keita-makino <kemakino@ucdavis.edu>
2020-02-06 01:01:01 -0800

    initial

---
+++ src/components/templates/ContentsPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const ContentsPanel: React.FC<Props> = (props: Props) => {
+  return <></>;
+};
+
+export default ContentsPanel;
---
+++ src/components/templates/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppBar } from '@material-ui/core';
+
+type Props = {};
+
+const Header: React.FC<Props> = (props: Props) => {
+  return <AppBar />;
+};
+
+export default Header;
